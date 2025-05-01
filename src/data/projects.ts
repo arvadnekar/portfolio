25530b8b8d3ed571e98ebd6ff8027208a9a2a9e2
Akanksha Vadnekar <arvadnekar@mun.ca>
2025-05-01 12:26:15 -0230

    Added new projects.ts in data for cenetralized info

---
+++ src/data/projects.ts
@@ -0,0 +1,33 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Portfolio",
+        description: "A modern portfolio website built with Next.js, TypeScript, and Tailwind CSS. Features dark,light,system mode, and seamless animations.",
+        image: "",
+        tags: ["Next.js", "TypeScript", "Tailwind CSS", "ShadCN"],
+        liveUrl: "https://portfolio-jade-five-70.vercel.app/",
+        githubUrl: "https://github.com/arvadnekar/portfolio",
+        featured: true
+      },
+    {
+      id: 2,
+      title: "Facial Recognition Obfuscation Tool",
+      description: "A FastAPI-based server for secure video processing, including automated face blurring, hand landmark detection, and private video uploads. Uses OpenCV, YOLO, and AI-based detection to ensure privacy and security.",
+      image: "",
+      tags: ["FastAPI", "ML", "OpenCV", "YOLO", "Python", "Docker", "Hand Tracking"],
+      githubUrl: "https://github.com/arvadnekar/face-logo-blur",
+      featured: false
+    },
+    {
+        id: 3,
+        title: "Expense-Tracker-Mun",
+        description: "A smart financial management application to track income, expenses, and spending habits with real-time insights and categorized analysis.",
+        image: "",
+        tags: ["Next.js", "TypeScript", "Tailwind CSS","PostgreSQL", "Prisma", "ShadCN", "NextAuth"],
+        liveUrl: "https://expense-tracker-mun.vercel.app/",
+        githubUrl: "https://github.com/idarshan181/ExpenseTracker-MUN",
+        featured: false
+      },
+    ];
+
+    
